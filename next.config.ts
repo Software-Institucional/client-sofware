98eebe7284cdc0b4f8a14ed20a387a5794d875be
sasamile <nspes2020@gmail.com>
2025-06-22 07:07:36 -0500

    feat:Update secure production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.eduadminsoft.shop/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
